8de22d7dc6a41403bf073b8ef71b94ce22dfd2d0
Akshat1927 <kardakakshat@gmail.com>
2026-07-02 16:15:57 +0530

    fix: replace undefined 'protect' with imported 'auth' middleware in authRoutes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,13 +1,12 @@
 import express from "express";
-import { getUser, login, register } from "../controllers/authController.js";
+import { getUser, login, register, updateSchedule } from "../controllers/authController.js";
 import auth from "../middleware/auth.js";
-import { updateSchedule } from "../controllers/authController.js";
 
 const authRouter = express.Router();
 
 authRouter.post('/register', register);
 authRouter.post('/login', login);
 authRouter.get('/user', auth, getUser);
-authRouter.put("/schedule", protect, updateSchedule);
+authRouter.put("/schedule", auth, updateSchedule);
 
 export default authRouter;
